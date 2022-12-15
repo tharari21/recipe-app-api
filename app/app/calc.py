a46108c3e955bd7c6e08dc9ce8fd3631aa708494
Tomer <tharari21@gmail.com>
2022-12-15 15:37:14 -0500

    Added db service with postgresql adapter installed on docker image

--- app/app/calc.py
+++ app/app/calc.py
@@ -4,3 +4,7 @@ Calculator functions
 def add(x, y):
     """Add x and y then return result"""
     return x + y
+
+def subtract(x, y):
+    """Subtract y from x then return result"""
+    return y - x
